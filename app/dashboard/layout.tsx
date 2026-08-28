a46ce58e844914fd43625ce669ba2e443885786b
ssr-naveen <ssr.naveen@hotmail.com>
2026-08-28 22:09:40 +0530

    Remove duplicate mobile dashboard menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,14 +4,13 @@ import "./mobile-nav.css";
 import "./desktop-sidebar.css";
 import "./admin/users/user-management.css";
 import "katex/dist/katex.min.css";
-import { MobileMenu } from "./MobileSidebar";
 import DesktopSidebarToggle from "./DesktopSidebarToggle";
 
 export const dynamic = "force-dynamic";
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return <>
-    <MobileMenu />
     {children}
     <DesktopSidebarToggle />
   </>;
 }
+
